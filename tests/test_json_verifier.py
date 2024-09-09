2f6301760a03feb04289cf2a804ef42886c16015
Hai Vu <haivu2004@gmail.com>
2024-09-09 15:36:24 -0700

    Initial

---
+++ tests/test_json_verifier.py
@@ -0,0 +1,42 @@
+import pytest
+
+from json_verifier import JsonVerifier
+
+
+@pytest.fixture
+def verifier():
+    actual = {"metadata": {"name": "sandbox", "tags": ["scratch", "pre-production", "experimental"]}}
+    return JsonVerifier(actual)
+
+
+def test_expect_pass(verifier):
+    with verifier:
+        verifier.verify_value("metadata.name", "sandbox")
+        verifier.verify_value("metadata.tags.0", "scratch")
+        verifier.verify_value("metadata.tags.1", "pre-production")
+
+
+@pytest.mark.xfail
+def test_expect_fail(verifier):
+    with verifier:
+        verifier.verify_value("metadata.name", "staging")
+        verifier.verify_value("metadata.description", "Team sandbox")
+        verifier.verify_value("metadata.tags.0", "testing")
+        verifier.verify_value("metadata.tags.3", "non-production")
+
+
+def test_verify_value(verifier):
+    verifier.verify_value("metadata.name", "staging")
+    verifier.verify_value("metadata.description", "Team sandbox")
+    verifier.verify_value("metadata.tags.0", "testing")
+    verifier.verify_value("metadata.tags.3", "non-production")
+
+    assert verifier.errors == [
+        "path='metadata.name', expected='staging', actual='sandbox'",
+        "path='metadata.description', expected='Team sandbox', key error: description",
+        "path='metadata.tags.0', expected='testing', actual='scratch'",
+        "path='metadata.tags.3', expected='non-production', index error: 3",
+    ]
+
+    with pytest.raises(AssertionError):
+        verifier.tally()
